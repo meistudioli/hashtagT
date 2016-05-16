8444c24cb2827f94352ff7bc75f5d8b7d0a01d06
Paul <meistudioli@gmail.com>
2016-05-16 11:46:25 +0800

    remove useless

--- hashtagTFunc.php
+++ hashtagTFunc.php
@@ -1,7 +1,6 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Credentials: true");
-/*header("Content-Type: text/html; charset=utf-8");*/
 header("Content-type: application/json; charset=utf-8");
 header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
 header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
@@ -39,59 +38,5 @@ switch ($action) {
 	default:
 		$libs->callBack('queryError', array(array('errCode'=>'', 'errMsg'=>'Unexcept error occurs. Try it later, please.')));
 }//end switch
-
-
-
-
-
-
-
-
-
-if (isset($callBack)) sleep(1);
-
-$FilePath = './illuTransLog.inc';
-$file_content = '';
-foreach ($_POST as $key => $value) {
-	$file_content .= $key.': '.urldecode($value)."\n";
-}//end for
-
-foreach ($_FILES as $Key => $Value) {
-	$file_content .= 'ColName: '.$Key."\n";
-	$file_content .= 'FileName: '.$Value['name']."\n";
-	$file_content .= 'FileSize: '.$Value['size']."\n";
-	$MovePath = str_replace('\\', '/', realpath("./").'/inputFiles/'.$Value['name']);
-	$file_content .= 'RealPath: '.$MovePath."\n\n";
-	move_uploaded_file($Value['tmp_name'], $MovePath);
-}//end for
-
-$FH = fopen($FilePath, "a+");
-fwrite($FH, $file_content);
-fclose($FH);
-
-if (isset($jojo)) {
-	$key = rand(0, 18);
-	$DataArray = array();
-	$DataArray[] = array(
-						'icon' => '/Modules/img/illuTrans/shiseido_jojo_'.$key.'_150.jpg',//must have
-						'preview' => '/Modules/img/illuTrans/shiseido_jojo_'.$key.'.jpg',//must have
-						'id' => 'mei'
-					);
-} else {
-	$key = rand(0, 8);
-	// $key = 1;
-	$DataArray = array();
-	$DataArray[] = array(
-						'icon' => '/Modules/img/illuTrans/anri_'.$key.'_150.jpg',//must have
-						'preview' => '/Modules/img/illuTrans/anri_'.$key.'.jpg',//must have
-						'id' => 'mei'
-					);
-}//end if
-
-// $status = (rand(0, 1)) ? 'success' : 'fail';
-// $status = 'success';
-$status = (!isset($err)) ? 'success' : ((rand(0, 1)) ? 'success' : 'fail');
-// $status = 'fail';
-$libs->callBack($status, $DataArray);
 /*中文*/
 ?>
